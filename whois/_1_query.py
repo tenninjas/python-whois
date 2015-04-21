61bb908afd4b83e5a52667f66e87ab4a42cf835f
tenninjas <tenninjas@tenninjas.ca>
2015-04-22 00:37:43 +0800

    Add feature for more robust querying. This allows for better results when referals are given, as well as avoiding poor responses from whois servers due to 'WHOIS SPAM' domains.

--- whois/_1_query.py
+++ whois/_1_query.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import sys
 import os
+import re
 
 PYTHON_VERSION = sys.version_info[0]
 CACHE = {}
@@ -31,25 +32,27 @@ def cache_save(cf):
     f.close()
 
 
-def do_query(dl, force=0, cache_file=None, slow_down=0, ignore_returncode=0):
+def do_query(dl, force=0, cache_file=None, slow_down=0, ignore_returncode=0, robust_query=0):
     k = '.'.join(dl)
     if cache_file: cache_load(cache_file)
     if force or k not in CACHE or CACHE[k][0] < time.time() - CACHE_MAX_AGE:
         CACHE[k] = (
         int(time.time()),
-        _do_whois_query(dl, ignore_returncode),
+        _do_whois_query(k, ignore_returncode, robust_query),
         )
         if cache_file: cache_save(cache_file)
         if slow_down: time.sleep(slow_down)
 
     return CACHE[k][1]
 
-
-def _do_whois_query(dl, ignore_returncode):
+def _do_whois_query(dq, ignore_returncode, robust_query):
     """
         Linux 'whois' command wrapper
     """
-    p = subprocess.Popen(['whois', '.'.join(dl)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if not type(dq) is list:
+        dq = [dq]
+    dq[:0] = ['whois']
+    p = subprocess.Popen(dq, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     r = p.communicate()[0]
     if PYTHON_VERSION == 3:
         try:
@@ -58,6 +61,21 @@ def _do_whois_query(dl, ignore_returncode):
             r = r.decode('ISO-8859-1')
 
     if not ignore_returncode and p.returncode != 0: raise Exception(r)
+
+    if robust_query:
+        rq = re.findall(r'single out[^"]+"xxx"', r, re.IGNORECASE)
+        if rq:
+            # multiple records present, be more specific
+            r = _do_whois_query(['domain {:s}'.format(dq[1])], ignore_returncode, robust_query)
+        rq = re.findall(r'Invalid query', r, re.IGNORECASE)
+        if rq:
+            # Pass query to a specific referal server as indicated in previous response
+            rq = re.findall(r'Whois Server:\s{0,}(.+)', r, re.IGNORECASE)
+            try:
+                dq = re.search(r'domain\s(.+)', dq[1], re.IGNORECASE).group(1)
+            except AttributeError:
+                raise Exception('Cannot find domain part!')
+            r = _do_whois_query(['-h',rq[0],dq], ignore_returncode, robust_query)
     return r
 
 
